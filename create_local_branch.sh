984b0a3974b9241b66d5df0b9019ccc3508205d3
actionless <actionless.loveless@gmail.com>
2015-09-04 22:41:35 +0200

    chore: add 'create-local-branch' script

---
+++ create_local_branch.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euo pipefail
+
+branches=(
+	# local overrides
+	'actionless'
+
+	# my branches
+
+	# psychon
+
+	# other
+)
+
+cd ~/projects
+rm -r tmp-awesome/awesome -f || true
+cp -prf awesome tmp-awesome/
+
+cd tmp-awesome/awesome
+git reset --hard
+
+git checkout master
+git pull upstream master
+
+git branch -D local
+git checkout -b local
+
+for branch in ${branches[@]}; do
+	echo "======================"
+	echo " merge $branch "
+	git merge $branch -m "merge $branch"
+done
+
+git push origin local -f
+
+cd ~/build/awesome-git
+makepkg -fi
+
+cd ~/projects/awesome
