f8fc11e853464539a1edafeeec4c7afe0fd12ea4
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-04-03 04:33:37 -0400

    layout.dynamic: Add a minimap widget [TODO]

---
+++ lib/awful/layout/dynamic/minimap.lua
@@ -0,0 +1,61 @@
+---------------------------------------------------------------------------
+--- Generate a list of rectangles representing a layout, even when hidden
+--
+-- @author Emmanuel Lepage Vallee &lt;elv1313@gmail.com&gt;
+-- @copyright 2016 Emmanuel Lepage Vallee
+-- @release @AWESOME_VERSION@
+-- @module awful.layout.dynamic.minimap
+---------------------------------------------------------------------------
+
+-- local module = {}
+
+-- local function get_rects(handler)
+--     local ret = {}
+-- 
+--     --Move to the work area
+--     local workarea = handler.param.workarea
+-- 
+--     local add_x, add_y = workarea.x, workarea.y
+-- 
+--     local function handle_hierarchy(h)
+--         local widget = h:get_widget()
+--         if widget._client or widget.draw == draw then
+--             local matrix = h:get_matrix_to_device()
+--             local x, y = matrix:transform_point(0, 0)
+--             local width, height = h:get_size()
+--             table.insert(ret, {
+--                 x      = x + add_x,
+--                 y      = y + add_y,
+--                 width  = width,
+--                 height = height,
+--                 client = widget._client,
+--                 widget = widget
+--             })
+--         end
+-- 
+--         for _, child in ipairs(h:get_children()) do
+--             handle_hierarchy(child)
+--         end
+--     end
+-- 
+--     handle_hierarchy(handler.hierarchy)
+-- 
+--     return ret, workarea.width, workarea.height
+-- end
+
+-- local function scale_rects(rects, wa_w, wa_h, res_w, res_h)
+--     --TODO
+-- end
+
+-- WORK IN PROGRESS
+-- local function minimap(handler, width, height, floating)
+--     local rects, wa_w, wa_h = get_rects(handler)
+-- 
+--     for k, rect in ipairs(rects) do
+--         print("HELLO", rect.x, rect.x, rect.width, rect.height)
+--     end
+-- 
+--     print("\n\nGENERATING MINIMAP!!!!")
+-- end
+
+-- return setmetatable(module, { __call = function(_, ...) return minimap(...) end })
