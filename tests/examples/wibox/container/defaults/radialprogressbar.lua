e22f5db0f12dce9b94072a523cfe852607da61f7
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-07-25 00:16:37 -0400

    tests: Test the radical progressbar

---
+++ tests/examples/wibox/container/defaults/radialprogressbar.lua
@@ -0,0 +1,27 @@
+--DOC_HIDE_ALL
+local wibox     = require("wibox")
+local gears     = {shape = require("gears.shape")}
+local beautiful = require("beautiful")
+
+return {
+    text   = "Before",
+    align  = "center",
+    valign = "center",
+    widget = wibox.widget.textbox,
+},
+{
+    {
+        {
+            text   = "After",
+            align  = "center",
+            valign = "center",
+            widget = wibox.widget.textbox,
+        },
+        value              = 0.5,
+        max_value          = 1,
+        min_value          = 0,
+        widget             = wibox.container.radialprogressbar
+    },
+    margins = 5,
+    layout = wibox.container.margin
+}
---
+++ tests/examples/wibox/container/radialprogressbar/value.lua
@@ -0,0 +1,30 @@
+--DOC_HIDE_ALL
+local parent    = ...
+local wibox     = require("wibox")
+local gears     = {shape = require("gears.shape")}
+local beautiful = require("beautiful")
+
+local function gen(val)
+    return wibox.widget {
+        {
+            text   = "Value: "..(val*100).."%",
+            align  = "center",
+            valign = "center",
+            widget = wibox.widget.textbox,
+        },
+        padding            = 5,
+        value              = val,
+        max_value          = 1,
+        min_value          = 0,
+        widget             = wibox.container.radialprogressbar
+    }
+end
+
+local l = wibox.layout {
+    gen(0.0),gen(0.3),gen(1.0),
+    forced_height = 30,
+    forced_width  = 400,
+    layout = wibox.layout.flex.horizontal
+}
+
+parent:add(l)
