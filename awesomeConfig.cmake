fa7d97de03c27cdb7421226f9bc7bd3d466bbfae
actionless <actionless.loveless@gmail.com>
2016-10-06 21:26:06 +0200

    fix(cmake): ignore ldoc result

--- awesomeConfig.cmake
+++ awesomeConfig.cmake
@@ -42,6 +42,14 @@ a_find_program(LDOC_EXECUTABLE ldoc FALSE)
 if(NOT LDOC_EXECUTABLE)
     a_find_program(LDOC_EXECUTABLE ldoc.lua FALSE)
 endif()
+if(LDOC_EXECUTABLE)
+    execute_process(COMMAND sh -c "${LDOC_EXECUTABLE} --sadly-ldoc-has-no-version-option 2>&1  | grep ' vs 1.4.4'"
+                    OUTPUT_VARIABLE LDOC_VERSION_RESULT)
+    if(NOT LDOC_VERSION_RESULT STREQUAL "")
+        message(WARNING "Ignoring LDoc, because version 1.4.4 is known to be broken")
+        unset(LDOC_EXECUTABLE)
+    endif()
+endif()
 # theme graphics
 a_find_program(CONVERT_EXECUTABLE convert TRUE)
 # pkg-config
