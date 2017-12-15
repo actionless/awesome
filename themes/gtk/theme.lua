d57c20353179dd8ced6d2d3f804a68c59cbb5aa4
actionless <actionless.loveless@gmail.com>
2017-12-15 03:43:26 +0100

    fix(themes: gtk): adapt color functions to work with hex rgba

--- themes/gtk/theme.lua
+++ themes/gtk/theme.lua
@@ -80,18 +80,28 @@ theme.menu_border_width = theme.gtk.border_width
 local hex_color_match = "[a-fA-F0-9][a-fA-F0-9]"
 local function darker(color_value, darker_n)
     local result = "#"
+    local channel_counter = 1
     for s in color_value:gmatch(hex_color_match) do
-        local bg_numeric_value = tonumber("0x"..s) - darker_n
+        local bg_numeric_value = tonumber("0x"..s)
+        if channel_counter <= 3 then
+            bg_numeric_value = bg_numeric_value - darker_n
+        end
         if bg_numeric_value < 0 then bg_numeric_value = 0 end
         if bg_numeric_value > 255 then bg_numeric_value = 255 end
         result = result .. string.format("%02x", bg_numeric_value)
+        channel_counter = channel_counter + 1
     end
     return result
 end
 local function is_dark(color_value)
     local bg_numeric_value = 0;
+    local channel_counter = 1
     for s in color_value:gmatch(hex_color_match) do
         bg_numeric_value = bg_numeric_value + tonumber("0x"..s);
+        if channel_counter == 3 then
+            break
+        end
+        channel_counter = channel_counter + 1
     end
     local is_dark_bg = (bg_numeric_value < 383)
     return is_dark_bg
