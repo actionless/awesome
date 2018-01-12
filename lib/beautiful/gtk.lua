941618605fa75accaeccc59231585c23a4978577
actionless <actionless.loveless@gmail.com>
2018-01-12 23:14:57 +0100

    wip(beautiful: gtk): get more style variables from the gtk

--- lib/beautiful/gtk.lua
+++ lib/beautiful/gtk.lua
@@ -7,6 +7,7 @@
 ---------------------------------------------------------------------------
 local gears_debug = require("gears.debug")
 local gears_math = require("gears.math")
+local gears_surface = require("gears.surface")
 local join = require("gears.table").join
 local unpack = unpack or table.unpack -- luacheck: globals unpack (compatibility with Lua 5.1)
 
@@ -36,13 +37,19 @@ local function lookup_gtk_color_to_hex(_style_context, color_name)
   return convert_gtk_color_to_hex(gtk_color)
 end
 
-local function get_gtk_color_property_to_hex(_style_context, property_name)
+local function get_gtk_property(_style_context, property_name)
   local state = _style_context:get_state()
   local property = _style_context:get_property(property_name, state)
   if not property then
     return nil
   end
-  return convert_gtk_color_to_hex(property.value)
+  return property.value
+end
+
+local function get_gtk_color_property_to_hex(_style_context, property_name)
+  return convert_gtk_color_to_hex(
+    get_gtk_property(_style_context, property_name)
+  )
 end
 
 local function read_gtk_color_properties_from_widget(gtk_widget, properties)
@@ -87,17 +94,21 @@ function gtk.get_theme_variables()
   end
   local style_context = window:get_style_context()
 
-  local font = style_context:get_font(Gtk.StateFlags.NORMAL)
-  --result.font_family = font:get_family()
-  --result.font_size = font:get_size()/1024
-  result.font = font:to_string()
-
   result = join(result, read_gtk_color_properties_from_widget(
     window, {
       bg_color="background-color",
       fg_color="color",
     }
   ))
+  local label = Gtk.Label()
+  local label_style_context = label:get_style_context()
+  result["font_size"] = get_gtk_property(
+    label_style_context, "font-size"
+  ) / (1+1/3)  -- i have no clue why it's 1.(3) times bigger than real
+  result["font_family"] = get_gtk_property(
+    label_style_context, "font-family"
+  )[1]
+
   result = join(result, read_gtk_color_properties_from_widget(
     Gtk.Entry(), {
       base_color="background-color",
@@ -115,6 +126,7 @@ function gtk.get_theme_variables()
     button, {
       button_bg_color="background-color",
       button_fg_color="color",
+      button_border_color="border-color",
     }
   ))
   local button_style_context = button:get_style_context()
@@ -127,6 +139,77 @@ function gtk.get_theme_variables()
     result[result_key] = property.value
     property:unset()
   end
+
+  local headerbar = Gtk.HeaderBar()
+  window:set_titlebar(headerbar)
+  result = join(result, read_gtk_color_properties_from_widget(
+    headerbar, {
+      menubar_bg_color="background-color",
+    }
+  ))
+
+  -- @TODO: remove this debug block
+  ---------------------------------------------------------------------
+  local headerbar_style_context = headerbar:get_style_context()
+  local cairo = require('lgi').cairo
+  local menubar_bg_image_surfacepattern = get_gtk_property(
+    headerbar_style_context, "background-image"
+  )
+  if menubar_bg_image_surfacepattern then
+    local surface = gears_surface.duplicate_surface(
+      cairo.ImageSurface.create(cairo.Format.ARGB32, 22, 22)
+    )
+    menubar_bg_image_surfacepattern:get_surface(surface)
+    result.menubar_bg_image = surface
+    print(
+      --result.menubar_bg_image:get_rgba()
+      result.menubar_bg_image
+    )
+  end
+  ---------------------------------------------------------------------
+
+  -- @TODO: remove if condition here:
+  if result.menubar_bg_color and result.menubar_bg_color ~= "#00000000" then
+    headerbar:add(label)
+    result = join(result, read_gtk_color_properties_from_widget(
+      label, {
+        menubar_fg_color="color",
+      }
+    ))
+  end
+
+  headerbar:add(button)
+  result = join(result, read_gtk_color_properties_from_widget(
+    button, {
+      header_button_bg_color="background-color",
+      --header_button_fg_color="color",
+      header_button_border_color="border-color",
+    }
+  ))
+  -- @TODO: remove if condition here:
+  if result.header_button_bg_color and result.header_button_bg_color ~= "#00000000" then
+    result = join(result, read_gtk_color_properties_from_widget(
+      button, {
+        header_button_fg_color="color",
+      }
+    ))
+  end
+
+  local error_button = Gtk.Button()
+  error_button:get_style_context():add_class("destructive-action")
+  result = join(result, read_gtk_color_properties_from_widget(
+    error_button, {
+      error_bg_color="background-color",
+      error_fg_color="color",
+    }
+  ))
+
+  -- @TODO: remove this debug block
+  ---------------------------------------------------------------------
+  for k,v in pairs(result) do
+    print(k..' = '..tostring(v))
+  end
+  ---------------------------------------------------------------------
 
   for _, color_data in ipairs({
     {"bg_color", "theme_bg_color"},
@@ -156,10 +239,27 @@ function gtk.get_theme_variables()
     {"wm_border_unfocused_color", "wm_border_unfocused", "wm_bg_color"},
     {"wm_title_unfocused_color", "wm_title_unfocused", "menubar_bg_color"},
     {"wm_icons_unfocused_color", "wm_icons_unfocused", "menubar_fg_color"},
+    --
+    {"error_color", "error_color"},
+    {"error_bg_color", "error_bg_color", "error_color"},
+    {"error_fg_color", "error_fg_color", "selected_fg_color"},
+    {"error_color", "error_color", "error_bg_color"},
+    {"warning_color", "warning_color"},
+    {"warning_bg_color", "warning_bg_color", "warning_color"},
+    {"warning_fg_color", "warning_fg_color", "selected_fg_color"},
+    {"warning_color", "warning_color", "warning_bg_color"},
+    {"success_color", "success_color"},
+    {"success_bg_color", "success_bg_color", "success_color"},
+    {"success_fg_color", "success_fg_color", "selected_fg_color"},
+    {"success_color", "success_color", "success_bg_color"},
   }) do
     local result_key, style_context_key, fallback_key = unpack(color_data)
-    result[result_key] = lookup_gtk_color_to_hex(style_context, style_context_key) or
-      (result[result_key] ~= "#00000000" and result[result_key] or result[fallback_key] or result[result_key])
+    result[result_key] = lookup_gtk_color_to_hex(style_context, style_context_key) or (
+      result[result_key] ~= "#00000000" and
+      result[result_key] or
+      result[fallback_key] or
+      result[result_key]  -- <-- here is for case if it was meant to be a fully transparent color on purpose
+    )
     if not result[result_key] then
       gears_debug.print_warning("Can't read color '" .. style_context_key .. "' from GTK+3 theme.")
     end
